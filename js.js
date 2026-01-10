d03019af123a8e77f13ac7cd51310a3e193055c7
Saneesh NT <148613852+saneeshnt@users.noreply.github.com>
2026-01-10 16:43:05 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+ function handleClick(){
+            document.querySelector("body>button").innerText="in progress "
+                        document.getElementById("heading").style.color="red"
+
+        }
