96428a21a69bbf984abfd278201b8f36820e5668
Saneesh NT <148613852+saneeshnt@users.noreply.github.com>
2026-01-12 18:17:22 +0530

    Add files via upload

---
+++ JsDay1.js
@@ -0,0 +1,8 @@
+console.log(1234)
+function handleclick(){
+    document.querySelector("body>button")
+   // document.getElementById("heading").style.color="red"
+    document.querySelector("body>button")
+    document.getElementById("heading").style.color="green"
+
+}
